766d4b4795774ab9811ba4056f0027a884dd6600
Chad A. Davis <chad.a.davis@gmail.com>
2021-02-07 19:04:48 +0100

    Anki Add: options -w to wildcard search and -b to search card back

--- python/ankia.py
+++ python/ankia.py
@@ -29,101 +29,115 @@ from optparse import OptionParser
 def request(action, **params):
     return {'action': action, 'params': params, 'version': 6}
 
+
 def invoke(action, **params):
-    requestJson=json.dumps(request(action, **params)).encode('utf-8')
-    response=json.load(urllib.request.urlopen(
+    requestJson = json.dumps(request(action, **params)).encode('utf-8')
+    response = json.load(urllib.request.urlopen(
         urllib.request.Request('http://localhost:8765', requestJson)))
     if response['error'] is not None:
         raise Exception(response['error'])
     return response['result']
 
-parser = OptionParser()
-parser.add_option(
-    "-y", "--sync", dest="sync", action="store_true", help="Sync DB to Anki Web and exit",
-    )
-# TODO option for only exact match, or to disable searching wildcard or back, etc
 
-(opts, args)=parser.parse_args()
-if opts.sync:
+parser = OptionParser()
+parser.add_option("-y", "--sync", dest="sync",
+                  action="store_true", help="Sync DB to Anki Web and exit")
+parser.add_option("-w", "--wild", dest="wild", action="store_true",
+                  help="Do wildcard search (of card front)")
+parser.add_option("-b", "--back", dest="back", action="store_true",
+                  help="Do (wildcard) search of card back")
+
+(opt, args) = parser.parse_args()
+if opt.sync:
     invoke('sync')
     exit()
 
-
-deck='nl'
-term=len(sys.argv) > 1 and ' '.join(sys.argv[1:])
-# print(f"term='{term}'")
-term=term or input("Find or create term: ")
+# Check remaining CLI args for term(s)
+term = len(args) and ' '.join(args)
+term = term or input("Find or create term: ")
+deck = 'nl'
 # TODO for searching, can't have a ' ' char, for some reason,
 # but we also don't want to the newly created 'front' field to have '_' in it
 # So make a separate search_term with '_' in it, and maybe also canonicalize whitespace here
-search_term=re.sub(r' ', '_', term)
-wild=f'*{search_term}*'
-field='front'
-result_exact=invoke('findCards', query = f'deck:{deck} {field}:{term}')
+search_term = re.sub(r' ', '_', term)
+wild = f'*{search_term}*'
+field = 'front'
+result_exact = invoke('findCards', query=f'deck:{deck} {field}:{term}')
 # Keep track of whether this card already exists
-results=result_exact
+results = result_exact
 
 # TODO CLI option to also search wildcard, even if exact match
-if not results:
-    print("No matches. Searching for wildcard matches:")
-    result=invoke('findCards', query = f'deck:{deck} {field}:{wild}')
+if not results or opt.wild:
+    print("Searching for wildcard matches:")
+    # No need to append / prepend, because the wildcard match will include any exact match
+    results = invoke('findCards', query=f'deck:{deck} {field}:{wild}')
 
 # TODO CLI option to also search definitions, even if prev match
-if not results:
+if not results or opt.back:
     print("No matches. Searching in definitions:")
-    field='back'
-    results=invoke('findCards', query = f'deck:{deck} {field}:{wild}')
+    field = 'back'
+    # Prepend, so that front matches / exact matches are at the bottom of the screen output
+    results = invoke(
+        'findCards', query=f'deck:{deck} {field}:{wild}') + results
 
 # TODO wrap this for loop in a while/REPL
 # TODO make a CLI option to delete (or edit?) a card by ID, for debugging
 # That could just be part of the REPL after rendering a (set of?) card
 for card_id in results:
-    cardsInfo=invoke('cardsInfo', cards = [card_id])
-    card=cardsInfo[0]
+    cardsInfo = invoke('cardsInfo', cards=[card_id])
+    card = cardsInfo[0]
     print('=' * 80)
-    f=card['fields']['Front']['value']
-    print(f)
+    f = card['fields']['Front']['value']
+
     # TODO warn when f contains HTML, and prompt to open in browser, to clean it?
     # But I can't see it in the GUI, since WYSIWYG
     # Auto replace, and use the updateNoteFields API? (after prompting)
     # https://github.com/FooSoft/anki-connect/blob/master/actions/notes.md
-    b=card['fields']['Back']['value']
-    # TODO render HTML another way? eg as Markdown instead?
-    b=re.sub(r'&nbsp;', ' ', b)
-    # Remove tags that are usually in the phonetic markup
-    b=re.sub(r'\<\/?a.*?\>', '', b)
-    # Replace opening tags with a newline, since usually a new section
-    b=re.sub(r'\<[^/].*?\>', '\n', b)
-    # Remove remaining tags
-    b=re.sub(r'\<.*?\>', '', b)
-    # Max 2x newlines in a row
-    b=re.sub(r'\n{3,}', '\n\n', b)
+
     # TODO highlight 'term' in output (console colors). Use colorama
-    LTYELLOW="\033[1;33m"
-    LTRED="\033[1;31m"
-    NOSTYLE="\033[0;0m"
-    b=re.sub(term, f"{LTRED}{term}{NOSTYLE}", b)
+    LTYELLOW = "\033[1;33m"
+    LTRED = "\033[1;31m"
+    NOSTYLE = "\033[0;0m"
+    f = re.sub(term, f"{LTRED}{term}{NOSTYLE}", f)
+    print(f)
+
+    b = card['fields']['Back']['value']
+
+    # TODO render HTML another way? eg as Markdown instead?
+    b = re.sub(r'&nbsp;', ' ', b)
+    # Remove tags that are usually in the phonetic markup
+    b = re.sub(r'\<\/?a.*?\>', '', b)
+    # Replace opening tags with a newline, since usually a new section
+    b = re.sub(r'\<[^/].*?\>', '\n', b)
+    # Remove remaining tags
+    b = re.sub(r'\<.*?\>', '', b)
+    # Max 2x newlines in a row
+    b = re.sub(r'\n{3,}', '\n\n', b)
+
+    # TODO highlight 'term' in output (console colors). Use colorama
+    LTYELLOW = "\033[1;33m"
+    LTRED = "\033[1;31m"
+    NOSTYLE = "\033[0;0m"
+    b = re.sub(term, f"{LTRED}{term}{NOSTYLE}", b)
     print(b)
 
-# TODO 
-# Or rather prompt to fetch or not
+# TODO if not opt.fetch (make it default to True, but allow --no-fetch to disable ?)
+# Or better to prompt, if we're making a REPL ?
 if not result_exact:
-    url='http://www.woorden.org/woord/' + term
+    url = 'http://www.woorden.org/woord/' + term
     print(f"No exact match. Fetching: {url}")
     # This service does an exact match for {term}
-    content=urllib.request.urlopen(
+    content = urllib.request.urlopen(
         urllib.request.Request(url)).read().decode('utf-8')
     # TODO find something Devel::Comments to enable/disable debug mode printing
 
-# TODO extract smarter
-# Start from ... <div class="slider-wrap" style="padding:10px">
-# But not including © or rather "&copy" (but not always present)
-    match = re.search(f"(\<h2.*?{term}.*?)(?=&copy)", content)
+    # TODO extract smarter. Check DOM parsing libs
+    match = re.search(f"(\<h2.*?{term}.*?)(?=&copy|Bron:)", content)
     if not match:
         print("No matches.")
-        exit() # TODO wrap this in a def and just return / exception
+        exit()  # TODO wrap this in a def and just return / raise exception
     definition = match.group()
-    # Duplicate check (deck scope) enabled by default
+    # Note, duplicate check (deck scope) enabled by default
     note = {
         'deckName': 'nl',
         'modelName': 'Basic-nl',
@@ -132,4 +146,3 @@ if not result_exact:
     card_id = invoke('addNote', note=note)
     print(f"Added card: {card_id}")
     # TODO call def to fetch and display this newly added exact match
-

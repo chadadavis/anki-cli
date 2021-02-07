4cc27f1e4fe38817dc68ad2a0edfad95e2278a5e
Chad A. Davis <chad.a.davis@gmail.com>
2021-02-07 21:18:50 +0100

    Anki Add: refactor def render, and more CLI options, parsing is WIP ...

--- python/ankia.py
+++ python/ankia.py
@@ -39,6 +39,27 @@ def invoke(action, **params):
     return response['result']
 
 
+def render(string, highlight=None):
+    # TODO render HTML another way? eg as Markdown instead?
+    string = re.sub(r'&nbsp;', ' ', string)
+    # Remove tags that are usually in the phonetic markup
+    string = re.sub(r'\<\/?a.*?\>', '', string)
+    # Replace opening tags with a newline, since usually a new section
+    string = re.sub(r'\<[^/].*?\>', '\n', string)
+    # Remove remaining tags
+    string = re.sub(r'\<.*?\>', '', string)
+    # Max 2x newlines in a row
+    string = re.sub(r'\n{3,}', '\n\n', string)
+    if highlight:
+        # TODO Use colorama
+        LTYELLOW = "\033[1;33m"
+        LTRED = "\033[1;31m"
+        NOSTYLE = "\033[0;0m"
+        string = re.sub(highlight, f"{LTRED}{highlight}{NOSTYLE}", string)
+
+    return string
+
+
 parser = OptionParser()
 parser.add_option("-y", "--sync", dest="sync",
                   action="store_true", help="Sync DB to Anki Web and exit")
@@ -46,6 +67,10 @@ parser.add_option("-w", "--wild", dest="wild", action="store_true",
                   help="Do wildcard search (of card front)")
 parser.add_option("-b", "--back", dest="back", action="store_true",
                   help="Do (wildcard) search of card back")
+parser.add_option("-f", "--fetch", dest="fetch", action="store_true",
+                  help="Fetch defnition, even if card already exists")
+parser.add_option("-a", "--add", dest="add", action="store_true",
+                  help="Add any fetched defnition")
 
 (opt, args) = parser.parse_args()
 if opt.sync:
@@ -62,23 +87,22 @@ deck = 'nl'
 search_term = re.sub(r' ', '_', term)
 wild = f'*{search_term}*'
 field = 'front'
-result_exact = invoke('findCards', query=f'deck:{deck} {field}:{term}')
+result_exact = invoke('findCards', query=f'deck:{deck} {field}:{search_term}')
 # Keep track of whether this card already exists
 results = result_exact
 
 # TODO CLI option to also search wildcard, even if exact match
 if not results or opt.wild:
-    print("Searching for wildcard matches:")
+    # print("Searching for wildcard matches:")
     # No need to append / prepend, because the wildcard match will include any exact match
     results = invoke('findCards', query=f'deck:{deck} {field}:{wild}')
 
 # TODO CLI option to also search definitions, even if prev match
 if not results or opt.back:
-    print("No matches. Searching in definitions:")
+    # print("Searching in definitions:")
     field = 'back'
-    # Prepend, so that front matches / exact matches are at the bottom of the screen output
-    results = invoke(
-        'findCards', query=f'deck:{deck} {field}:{wild}') + results
+    # No need to append / prepend, because the wildcard match will include any exact match
+    results = invoke('findCards', query=f'deck:{deck} {field}:{wild}')
 
 # TODO wrap this for loop in a while/REPL
 # TODO make a CLI option to delete (or edit?) a card by ID, for debugging
@@ -86,53 +110,39 @@ if not results or opt.back:
 for card_id in results:
     cardsInfo = invoke('cardsInfo', cards=[card_id])
     card = cardsInfo[0]
-    print('=' * 80)
-    f = card['fields']['Front']['value']
 
     # TODO warn when f contains HTML, and prompt to open in browser, to clean it?
     # But I can't see it in the GUI, since WYSIWYG
     # Auto replace, and use the updateNoteFields API? (after prompting)
     # https://github.com/FooSoft/anki-connect/blob/master/actions/notes.md
-
-    # TODO highlight 'term' in output (console colors). Use colorama
-    LTYELLOW = "\033[1;33m"
-    LTRED = "\033[1;31m"
-    NOSTYLE = "\033[0;0m"
-    f = re.sub(term, f"{LTRED}{term}{NOSTYLE}", f)
-    print(f)
+    print('=' * 80)
+    f = card['fields']['Front']['value']
+    print(render(f, highlight=term))
 
     b = card['fields']['Back']['value']
-
-    # TODO render HTML another way? eg as Markdown instead?
-    b = re.sub(r'&nbsp;', ' ', b)
-    # Remove tags that are usually in the phonetic markup
-    b = re.sub(r'\<\/?a.*?\>', '', b)
-    # Replace opening tags with a newline, since usually a new section
-    b = re.sub(r'\<[^/].*?\>', '\n', b)
-    # Remove remaining tags
-    b = re.sub(r'\<.*?\>', '', b)
-    # Max 2x newlines in a row
-    b = re.sub(r'\n{3,}', '\n\n', b)
-
-    # TODO highlight 'term' in output (console colors). Use colorama
-    LTYELLOW = "\033[1;33m"
-    LTRED = "\033[1;31m"
-    NOSTYLE = "\033[0;0m"
-    b = re.sub(term, f"{LTRED}{term}{NOSTYLE}", b)
-    print(b)
+    print(render(b, highlight=term))
 
 # TODO if not opt.fetch (make it default to True, but allow --no-fetch to disable ?)
 # Or better to prompt, if we're making a REPL ?
-if not result_exact:
-    url = 'http://www.woorden.org/woord/' + term
-    print(f"No exact match. Fetching: {url}")
+if not result_exact or opt.fetch:
+    url = 'http://www.woorden.org/woord/' + re.sub(' ', ' ', term)
+    print('=' * 80)
+    print(f"Fetching: {url}")
     # This service does an exact match for {term}
     content = urllib.request.urlopen(
         urllib.request.Request(url)).read().decode('utf-8')
     # TODO find something Devel::Comments to enable/disable debug mode printing
 
     # TODO extract smarter. Check DOM parsing libs
-    match = re.search(f"(\<h2.*?{term}.*?)(?=&copy|Bron:)", content)
+    # Pages in diferent formats
+    # encyclo:     https://www.woorden.org/woord/hangertje
+    # urlencoding: https://www.woorden.org/woord/op zich
+    # none:        https://www.woorden.org/woord/spacen
+    # ?:           https://www.woorden.org/woord/backspacen #
+    # &copy:       http://www.woorden.org/woord/zien
+    # Bron:        
+    match = re.search(f"(\<h2.*?{term}.*?)(?=&copy|Bron:|\<div|\<\/div)", content)
+    # match = re.search(f'(\<h2.*?{term}.*?)(?=div)', content) # This should handle all cases (first new/closing div)
     if not match:
         print("No matches.")
         exit()  # TODO wrap this in a def and just return / raise exception
@@ -143,6 +153,9 @@ if not result_exact:
         'modelName': 'Basic-nl',
         'fields': {'Front': term, 'Back': definition},
     }
-    card_id = invoke('addNote', note=note)
-    print(f"Added card: {card_id}")
-    # TODO call def to fetch and display this newly added exact match
+    print(render(definition, highlight=term))
+
+    if opt.add:
+        card_id = invoke('addNote', note=note)
+        print(f"Added card: {card_id} :")
+        # TODO call def to fetch and display this newly added card (to verify)
